a2f34f3ef32db7353f4712fc180a1529b848c50a
haewon96 <eunhae96@naver.com>
2021-01-07 17:36:03 +0900

    # 1003 : [기초-출력] 출력하기 03

---
+++ chap01/1003.py
@@ -0,0 +1,11 @@
+# 1003 : [기초-출력] 출력하기 03
+
+"""
+print('Hello')
+print('World')
+"""
+
+print('Hello', 'World', sep='\n')
+
+# end : print 함수가 끝난 후 넣을 문자 지정    (기본값 : 계행 \n)
+# sep : print 함수의 값들 사이에 넣을 문자 지정 (기본값 : 공백 1칸)
